df02895b9592b0ee72abb3b2d72db632b10b9251
Reno Crolla <reno.crolla@icloud.com>
2023-09-01 20:23:35 +0200

    Add new WeeklyWeatherData component to the Search page

--- src/pages/search/Search.js
+++ src/pages/search/Search.js
@@ -2,8 +2,11 @@ import React, {useState} from "react";
 import './Search.css';
 import SearchBox from "../../components/searchBox/SearchBox";
 import WeatherData from "../../components/weatherData/WeatherData";
+import ToggleButton from "../../components/weeklyWeatherToggle/ToggleButton";
+import WeeklyWeatherData from "../../components/weeklyWeatherData/WeeklyWeatherData";
 
 function Search() {
+  const [weeklyToggle, setWeeklyToggle] = useState(false);
   const [location1, setLocation1] = useState("");
   const [location2, setLocation2] = useState("");
   const [location3, setLocation3] = useState("");
@@ -11,28 +14,36 @@ function Search() {
   const [location5, setLocation5] = useState("");
 
   return (
-    <section id="multi-search-container">
-      <article className="search-container">
-        <SearchBox setLocationHandler= {setLocation1} />
-        <WeatherData locationName = {location1} />
-      </article>
-      <article className="search-container">
-        <SearchBox setLocationHandler= {setLocation2}/>
-        <WeatherData locationName = {location2} />
-      </article>
-      <article className="search-container">
-        <SearchBox setLocationHandler= {setLocation3}/>
-        <WeatherData locationName = {location3} />
-      </article>
-      <article className="search-container">
-        <SearchBox setLocationHandler= {setLocation4}/>
-        <WeatherData locationName = {location4} />
-      </article>
-      <article className="search-container">
-        <SearchBox setLocationHandler= {setLocation5}/>
-        <WeatherData locationName = {location5} />
-      </article>
-    </section>
+    <section>
+      <ToggleButton setToggleButton={setWeeklyToggle} />
+      <div id="multi-search-container">
+        <article className="search-container">
+          <SearchBox setLocationHandler= {setLocation1} />
+          {!weeklyToggle && <WeatherData locationName = {location1} />}
+          {weeklyToggle && location1 && <WeeklyWeatherData locationName = {location1} />}
+        </article>
+        <article className="search-container">
+          <SearchBox setLocationHandler= {setLocation2}/>
+          {!weeklyToggle && <WeatherData locationName = {location2} />}
+          {weeklyToggle && location2 && <WeeklyWeatherData locationName = {location2} />}
+        </article>
+        <article className="search-container">
+          <SearchBox setLocationHandler= {setLocation3}/>
+          {!weeklyToggle && <WeatherData locationName = {location3} />}
+          {weeklyToggle && location3 && <WeeklyWeatherData locationName = {location3} />}
+        </article>
+        <article className="search-container">
+          <SearchBox setLocationHandler= {setLocation4}/>
+          {!weeklyToggle && <WeatherData locationName = {location4} />}
+          {weeklyToggle && location4 && <WeeklyWeatherData locationName = {location4} />}
+        </article>
+        <article className="search-container">
+          <SearchBox setLocationHandler= {setLocation5}/>
+          {!weeklyToggle && <WeatherData locationName = {location5} />}
+          {weeklyToggle && location5 && <WeeklyWeatherData locationName = {location5} />}
+        </article>
+      </div>
+      </section>
   );
 }
 
